29bbcc6b2dab7558ab87d6c14b16be775b8e24ea
Egor Ostryakov <eg.ostr@mail.ru>
2022-03-18 22:04:26 +0300

    add transactions

--- src/main/java/blps/labs/service/ReviewService.java
+++ src/main/java/blps/labs/service/ReviewService.java
@@ -7,30 +7,43 @@ import blps.labs.repository.ReviewRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
 
 @Slf4j
 @Service
 public class ReviewService {
+
+    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate transactionTemplate;
     private final ReviewRepository reviewRepository;
     private final CarService carService;
 
     @Autowired
-    public ReviewService(ReviewRepository reviewRepository, CarService carService) {
+    public ReviewService(ReviewRepository reviewRepository, CarService carService, PlatformTransactionManager transactionManager) {
         this.reviewRepository = reviewRepository;
         this.carService = carService;
+        this.transactionManager = transactionManager;
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
     }
 
     public void saveReview(Review review) {
-        try {
-            Car carFromDB = carService.findCar(review.getCar());
-            review.setCar(carFromDB);
-        } catch (DataNotFoundException ignored) {
-        } finally {
-            reviewRepository.save(review);
-        }
-        log.debug("{} review by {} saved in DB", review.getCar().getCarModel(), review.getAuthorName());
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                try {
+                    Car carFromDB = carService.findCar(review.getCar());
+                    review.setCar(carFromDB);
+                } catch (DataNotFoundException ignored) {
+                } finally {
+                    reviewRepository.save(review);
+                }
+            }
+        });
     }
 
     public Review findReviewById(Long id) {
@@ -51,11 +64,16 @@ public class ReviewService {
     }
 
     public void changeApproval(Long id, Boolean approved) {
-        Review review = findReviewById(id);
-        if (review.isApproved() == approved)
-            return;
-        review.setApproved(approved);
-        saveReview(review);
-        log.debug("{} review with changed approval {} saved in DB", review.getCar().getCarModel(), review.getAuthorName());
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                Review review = findReviewById(id);
+                if (review.isApproved() == approved)
+                    return;
+                review.setApproved(approved);
+                saveReview(review);
+                log.debug("{} review with changed approval {} saved in DB", review.getCar().getCarModel(), review.getAuthorName());
+            }
+        });
     }
 }
