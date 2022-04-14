5fa1c7d99641ecd6944f00a6aca2a0ddb2f60f0e
Egor Ostryakov <eg.ostr@mail.ru>
2022-04-15 00:01:42 +0300

    add default constructor

--- MainApp/src/main/java/blps/labs/message/model/AddReviewMessage.java
+++ MainApp/src/main/java/blps/labs/message/model/AddReviewMessage.java
@@ -2,10 +2,12 @@ package blps.labs.message.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class AddReviewMessage implements Serializable {
     private long reviewId;
